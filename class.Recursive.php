68751d503e4de4a797dcf174534b09aa017ae0df
DEPIDSVY@BM4Z05J.nintendo.de <DEPIDSVY@BM4Z05J.nintendo.de>
2012-07-17 17:56:11 +0200

    - FIX menu working on ORS - FIX findInDB - FIX getControllerString

--- class.Recursive.php
+++ class.Recursive.php
@@ -19,4 +19,22 @@ class Recursive {
 		return $this->elements;
 	}
 
+	/**
+	 * @param array $path
+	 * @return Recursive
+	 */
+	function findPath(array $path) {
+		//debug($path);
+		if ($path) {
+			$current = array_shift($path);
+			$find = $this->elements[$current];
+			if ($find && $path) {
+				$find = $find->findPath($path);
+			}
+		} else {
+			$find = $this;	// Recursive
+		}
+		return $find;
+	}
+
 }
