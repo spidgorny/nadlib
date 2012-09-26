7c67e98aaa4408f26f00d9cc340c3e023e2c1992
DEPIDSVY <DEPIDSVY@BM4Z05J.nintendo.de>
2012-09-26 16:27:29 +0200

    - FIX FullGrid::getTableFieldOptions to use existing functions - ADD addJQueryUI - SIMPLIFY Scaffold: it no longer is meant for displaying list of items - ADD Scaffold::getDescFromThes - slTable options for the column and cell united - slTable can display HTMLFormType fields - ADD HTMLForm/class.HTMLFormDatePicker.php - MOVE $thes definition into OODBase - should be used in Collection as well

--- HTMLForm/class.HTMLFormType.php
+++ HTMLForm/class.HTMLFormType.php
@@ -7,6 +7,36 @@
 
 abstract class HTMLFormType {
 
+	/**
+	 * @var HTMLForm
+	 */
+	protected $form;
+
+	public $name;
+	public $fullname;
+	public $value;
+
+	function __construct() {
+	}
+
+	function setName($name) {
+		$this->name = $name;
+	}
+
+	function setForm(HTMLFormTable $f) {
+		$this->form = $f;
+		$this->fullname = $this->form->getName($this->name, '', TRUE);
+	}
+
+	/**
+	 * It's a string value which needs to be parsed into the minutes!!!
+	 *
+	 * @param unknown_type $value - 10:00-13:30
+	 */
+	function setValue($value) {
+		$this->value = $value;
+	}
+
 	abstract function render();
 
 }
