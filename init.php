6615298565992793c17fffa5eca1def7bebc64fd
DEPIDSVY@BM4Z05J.nintendo.de <DEPIDSVY@BM4Z05J.nintendo.de>
2013-01-29 17:48:11 +0100

    - ADD AutoLoad class

--- init.php
+++ init.php
@@ -1,55 +1,12 @@
 <?php
 
 function __autoload($class) {
-	if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->startTimer(__METHOD__);
-	//unset($_SESSION['autoloadCache']);
-	$folders = $_SESSION['autoloadCache'];
-	if (!$folders) {
-		require_once('class.ConfigBase.php');
-		if (file_exists($configPath = dirname($_SERVER['SCRIPT_FILENAME']).'/class/class.Config.php')) {
-			//echo($configPath);
-			include_once $configPath;
-		}
-		//echo($configPath);
-		if (class_exists('Config')) {
-			$folders = Config::$includeFolders
-				? array_merge(ConfigBase::$includeFolders, Config::$includeFolders)
-				: ConfigBase::$includeFolders;
-		} else {
-			$folders = ConfigBase::$includeFolders;
-		}
-		$_SESSION['autoloadCache'] = $folders;
+	require_once 'nadlib/class.AutoLoad.php';
+	static $a;
+	if (!$a) {
+		$a = new AutoLoad();
 	}
-
-	$namespaces = explode('\\', $class);
-	$classFile = end($namespaces);
-	$subFolders = explode('/', $classFile);		// Download/GetAllRoutes
-	$classFile = array_pop($subFolders);		// [Download, GetAllRoutes]
-	$subFolders = implode('/', $subFolders);	// Download
-	foreach ($folders as $path) {
-		$file = dirname(__FILE__).DIRECTORY_SEPARATOR.
-				$path.DIRECTORY_SEPARATOR.
-				$subFolders.DIRECTORY_SEPARATOR.
-				'class.'.$classFile.'.php';
-		if (file_exists($file)) {
-			$debug[] = $class.' <span style="color: green;">'.$file.'</span><br />';
-			include_once($file);
-			break;
-		} else {
-			$debug[] = $class.' <span style="color: red;">'.$file.'</span>: '.file_exists($file).'<br />';
-		}
-	}
-	if (!class_exists($classFile) && !interface_exists($classFile)) {
-		//debug($folders);
-		if (class_exists('Config')) {
-			$config = Config::getInstance();
-			if ($config->config['autoload']['notFoundException']) {
-				debug($debug);
-				throw new Exception('Class '.$class.' ('.$file.') not found.');
-			}
-		}
-	}
-	if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->stopTimer(__METHOD__);
+	$a->load($class);
 }
 
 function initNADLIB() {
