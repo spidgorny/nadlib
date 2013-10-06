0dda492aa1a213953b830096af994ff714e3cb2b
Slawa <Slawa@HP>
2013-10-06 23:26:00 +0200

    - ADD Profiler->startTimer everywhere - CHANGE all colors to darker blue - ADD Polaroid image effect

--- HTMLForm/class.HTMLFormProcessor.php
+++ HTMLForm/class.HTMLFormProcessor.php
@@ -45,6 +45,7 @@ abstract class HTMLFormProcessor extends AppController {
 	 * Who's gonna call this function? Index?
 	 */
 	function postInit() {
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->startTimer(__METHOD__);
 		$this->desc = $this->getDesc();
 		$this->form = $this->getForm();
 		//debug($this->desc);
@@ -67,6 +68,7 @@ abstract class HTMLFormProcessor extends AppController {
 			$this->form->importValues($this->default instanceof Request ? $this->default : new Request($this->default));
 			$this->desc = $this->form->desc;
 		}
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->stopTimer(__METHOD__);
 	}
 
 	abstract function getDesc();
@@ -76,6 +78,7 @@ abstract class HTMLFormProcessor extends AppController {
 	 * @return HTMLFormTable
 	 */
 	function render() {
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->startTimer(__METHOD__);
 		$content = '';
 		if (!$this->form) {
 			$this->postInit();
@@ -96,10 +99,12 @@ abstract class HTMLFormProcessor extends AppController {
 			$content .= $this->showForm();
 		}
 		$content = $this->encloseInAA($content, $this->title);
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->stopTimer(__METHOD__);
 		return $content;
 	}
 
 	function getForm(HTMLFormTable $preForm = NULL) {
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->startTimer(__METHOD__);
 		$f = $preForm ? $preForm : new HTMLFormTable($this->desc);
 		if ($this->ajax) {
 			$f->formMore = 'onsubmit="return ajaxSubmitForm(this);"';
@@ -107,10 +112,12 @@ abstract class HTMLFormProcessor extends AppController {
 		$f->method('POST');
 		$f->hidden('c', $this->prefix);
 		$f->hidden('ajax', $this->ajax);
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->stopTimer(__METHOD__);
 		return $f;
 	}
 
 	function showForm() {
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->startTimer(__METHOD__);
 		if (!$this->form) {
 			throw new Exception(__METHOD__.': initialize form with getForm()');
 		}
@@ -118,6 +125,7 @@ abstract class HTMLFormProcessor extends AppController {
 		$this->form->showForm();
 		$this->form->prefix('');
 		$this->form->submit($this->submitButton, array('class' => 'btn'));
+		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->stopTimer(__METHOD__);
 		return $this->form;
 	}
 
