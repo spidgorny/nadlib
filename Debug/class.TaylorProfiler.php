740764380629d5e946af4168b916fc32510e1561
DEPIDSVY <devnull@localhost>
2013-10-17 17:31:52 +0200

    - FIX LIVE constant detection for CLI

--- Debug/class.TaylorProfiler.php
+++ Debug/class.TaylorProfiler.php
@@ -309,7 +309,7 @@ class TaylorProfiler {
 
 	static function enableTick($ticker = 100) {
 		register_tick_function(array(__CLASS__, 'tick'));
-		declare(ticks=1000);
+		declare(ticks=100);
 	}
 
 	static function tick() {
@@ -324,7 +324,13 @@ class TaylorProfiler {
 		$mem = self::getMemUsage();
 		$diff = number_format(100*($mem - $prev), 2);
 		$diff = $diff > 0 ? '<font color="green">'.$diff.'</font>' : '<font color="red">'.$diff.'</font>';
-		echo '<pre>'.$diff.' '.number_format($mem*100, 2).'% '.implode(' // ', $list).'</pre>'."\n";
+		$trace = implode(' -> ', $list);
+		$trace = substr($trace, -80);
+		$output = '<pre>diff: '.$diff.' '.number_format($mem*100, 2).'% '.$trace.'</pre>';
+		if (Request::isCLI()) {
+			$output = strip_tags($output);
+		}
+		echo $output."\n";
 		$prev = $mem;
 	}
 
