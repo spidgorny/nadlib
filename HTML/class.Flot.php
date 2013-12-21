0431a309b510a3e487e81df67a2576595fe3656f
Slawa@HP <Slawa@HP>
2013-12-21 17:56:45 +0100

    - ADD Flot chart of programs per day in ChannelInfo - ADD composer.json for flot/flot - ADD moving average calculation to Flot

--- HTML/class.Flot.php
+++ HTML/class.Flot.php
@@ -15,8 +15,11 @@ class Flot extends AppController {
 	);
 
 	protected $data, $cumulative;
+	public $movingAverage;
 
 	protected $keyKey, $timeKey, $amountKey;
+
+	public $MALength = 20;
 
 	function __construct(array $data, $keyKey, $timeKey, $amountKey) {
 		$this->data = $data;
@@ -27,11 +30,33 @@ class Flot extends AppController {
 
 	function render() {
 		$content = '';
-		$chart = $this->getChartTable($this->data);
-		$this->cumulative = $this->getChartCumulative($chart);
-		$max = $this->getChartMax($this->cumulative);
-		$content .= $this->showChart('chart1', $chart, $this->cumulative, $max);
+		$charts = $this->getChartTable($this->data);
+		$content .= $this->showChart('chart1', $charts);
 		return $content;
+	}
+
+	function renderCumulative() {
+		$content = '';
+		$charts = $this->getChartTable($this->data);
+		$this->cumulative = $this->getChartCumulative($charts);
+		$content .= $this->showChart('chart1', $charts, $this->cumulative);
+		return $content;
+	}
+
+	/**
+	 * http://bytes.com/topic/php/answers/747586-calculate-moving-average
+	 * @return string
+	 */
+	function renderMovingAverage() {
+		$content = '';
+		$charts = $this->getChartTable($this->data);
+		$this->movingAverage = $this->getMovingAverage($charts);
+		$content .= $this->showChart('chart1', $charts, $this->movingAverage);
+		return $content;
+	}
+
+	function __toString() {
+		return $this->render();
 	}
 
 	function appendCumulative(array $data) {
@@ -57,9 +82,9 @@ class Flot extends AppController {
 	 * Each row is an assoc array with $timeKey keys and $amountKey values.
 	 *
 	 * @param array $rows
-	 * @param string $keyKey
-	 * @param string $timeKey
-	 * @param string $amountKey
+	 * @internal param string $keyKey
+	 * @internal param string $timeKey
+	 * @internal param string $amountKey
 	 * @return array
 	 */
 	function getChartTable(array $rows) {
@@ -74,23 +99,24 @@ class Flot extends AppController {
 				unset($rows[$i]);
 			}
 		}
+		//debug(__METHOD__, $chart);
 		return $chart;
 	}
 
-	function getChartCumulative(array $chart) {
-		foreach ($chart as &$sub) {
+	function getChartCumulative(array $charts) {
+		foreach ($charts as &$sub) {
 			$sum = 0;
-			foreach ($sub as &$val) {
-				$sum += $val[1];
-				$val[1] = $sum;
+			foreach ($sub as &$pair) {
+				$sum += $pair[1];
+				$pair[1] = $sum;
 			}
 		}
-		return $chart;
+		return $charts;
 	}
 
-	function getChartMax(array $chart) {
+	function getChartMax(array $charts) {
 		$max = 0;
-		foreach ($chart as $series) {
+		foreach ($charts as $series) {
 			foreach ($series as $pair) {
 				$max = max($max, $pair[1]);
 			}
@@ -98,12 +124,37 @@ class Flot extends AppController {
 		return $max;
 	}
 
-	function showChart($divID, array $charts, array $cumulative, $max) {
+	function getMovingAverage(array $charts) {
+		foreach ($charts as $s => &$series) {
+			$res = array();
+			foreach ($series as $pair) {
+				$res[$pair[0]] = $pair[1];
+			}
+
+			$i = 0;
+			foreach ($res as &$row) {
+				$slice = array_slice($res, max($i-$this->MALength+1, 0), $this->MALength);
+				$row = round(
+					array_sum($slice) /
+					count($slice), 4);
+				$i++;
+			}
+
+			foreach ($res as $key => &$val) {
+				$val = array($key, $val);
+			}
+			$series = $res;
+		}
+		return $charts;
+	}
+
+	function showChart($divID, array $charts, array $cumulative = array()) {
+		$max = $this->getChartMax($charts);
 		Index::getInstance()->addJQuery();
 		Index::getInstance()->footer['flot'] = '
 		<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="flot/excanvas.min.js"></script><![endif]-->
-    	<script language="javascript" type="text/javascript" src="flot/jquery.flot.js"></script>
-    	<script language="javascript" type="text/javascript" src="flot/jquery.flot.time.js"></script>';
+    	<script language="javascript" type="text/javascript" src="vendor/flot/flot/jquery.flot.js"></script>
+    	<script language="javascript" type="text/javascript" src="vendor/flot/flot/jquery.flot.time.js"></script>';
 
 		$content = '<div id="'.$divID.'" style="width: 950px; height:600px; border: solid 1px silver;"></div>';
 
@@ -114,24 +165,28 @@ class Flot extends AppController {
 				data: '.json_encode($array).',
 				bars: {
 					show: true,
-					barWidth: 24*60*60*1000*25,
+					barWidth: 24*60*60*1000*0.75,
 					align: "center"
 				}
 			};';
 		}
 
-		foreach ($cumulative as $key => &$rows) {
-			$array = $rows ? array_values($rows) : array();
-			$rows = 'var c'.$key.' = {
-				data: '.json_encode($array).',
-				lines: {
-					show: true,
-					fill: false
-				},
-				yaxis: 2
-			};';
+		if ($cumulative) {
+			foreach ($cumulative as $key => &$rows) {
+				$array = $rows ? array_values($rows) : array();
+				$rows = 'var c'.$key.' = {
+					data: '.json_encode($array).',
+					lines: {
+						show: true,
+						fill: false
+					},
+					yaxis: 2
+				};';
+			}
+		} else {
+			$cumulative = array('Daily' => 'var cDaily = {};');
 		}
-		$max *= 2;
+		//$max *= 2;
 
 		Index::getInstance()->footer[$divID] = '
     	<script type="text/javascript">
@@ -140,7 +195,7 @@ $(function () {
 	'.implode("\n", $cumulative).'
     $.plot($("#'.$divID.'"), [
     	d'.implode(", d", array_keys($charts)).',
-    	c'.implode(", c", array_keys($charts)).'
+    	c'.implode(", c", array_keys($cumulative)).'
     ], {
     	xaxis: {
     		mode: "time"
