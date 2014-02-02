dd3233a25b50567cf5173116d04987adbf1ab278
Slawa@HP <Slawa@HP>
2014-02-02 19:15:30 +0100

    - CHANGE IndexBase to use one big try catch - index.php doesn't need initController call anymore - ADD Request::redirectJS() - ADD Uploader to detect AJAX and react correctly without it

--- HTMLForm/class.HTMLFormProcessor.php
+++ HTMLForm/class.HTMLFormProcessor.php
@@ -53,8 +53,8 @@ abstract class HTMLFormProcessor extends AppController {
 	 */
 	function postInit() {
 		if (isset($GLOBALS['profiler'])) $GLOBALS['profiler']->startTimer(__METHOD__);
-		$this->form = $this->getForm();
 		$this->desc = $this->getDesc();
+		$this->form = $this->getForm();
 		//debug($this->desc);
 		//debug($this->prefix);
 		if ($this->submitted) {
