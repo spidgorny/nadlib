1c72c7cacc5e10b343a4c64da1cba810b741d50a
DEPIDSVY <devnull@localhost>
2014-03-18 11:53:14 +0100

    - ADD UTF-8 as default locale

--- HTML/class.ProgressBar.php
+++ HTML/class.ProgressBar.php
@@ -99,7 +99,7 @@ class ProgressBar {
 		$this->percentDone = $percentDone;
 		$text = $text ? $text : number_format($this->percentDone, $this->decimals, '.', '').'%';
 		if ($this->cli) {
-			echo "\r". $text  . ' '.$this->getCLIbar(); // \r first to preserver errors
+			echo "\r". $text  . "\t".$this->getCLIbar(); // \r first to preserver errors
 		} else {
 			print('
 			<script type="text/javascript">
