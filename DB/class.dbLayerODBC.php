a8b5cc367f312f0c83709ed5cbc79c577309b3f8
DEPIDSVY <devnull@localhost>
2014-03-18 14:41:35 +0100

    - DEBUG duplicate row in ODBC - ADD CompareDB2

--- DB/class.dbLayerODBC.php
+++ DB/class.dbLayerODBC.php
@@ -77,11 +77,13 @@ class dbLayerODBC extends dbLayerBase implements DBInterface {
 	function fetchAssoc($res) {
 		if ($this->connection) {
 			if (is_null($this->cursor)) {
-				return odbc_fetch_array($res);
+				$row = odbc_fetch_array($res);
 			} else {
 				// row numbering starts with 1 - @see php.net
-				return odbc_fetch_array($res, 1+$this->cursor++);
+				$row = odbc_fetch_array($res, 1+$this->cursor++);
 			}
+			//debug(__METHOD__, $this->cursor, $row);
+			return $row;
 		} else {
 			throw new Exception(__METHOD__);
 		}
