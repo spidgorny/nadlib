f9fb276d9f76b1274e97b72780fb70944405ee88
DEPIDSVY <devnull@localhost>
2014-07-07 15:56:55 +0200

    - START moveProject.php

--- Controller/class.IndexBase.php
+++ Controller/class.IndexBase.php
@@ -156,6 +156,18 @@ class IndexBase /*extends Controller*/ {	// infinite loop
 		return $render;
 	}
 
+	static function mergeStringArrayRecursive($render) {
+		if (is_array($render)) {
+			//$render = implode("\n", $render); // not recursive
+			$combined = '';
+			array_walk_recursive($render, function ($value, $key) use (&$combined) {
+				$combined .= $value."\n";
+			});
+			$render = $combined;
+		}
+		return $render;
+	}
+
 	function renderException(Exception $e) {
 		$content = '<div class="ui-state-error alert alert-error padding">
 			'.$e->getMessage();
