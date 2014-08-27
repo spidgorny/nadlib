3a3ccda079789585dfce0c11211a6e86f75a7616
Pedram <none@none>
2014-08-27 15:28:25 +0200

    adjust method name

--- HTMLForm/class.HTMLFormValidate.php
+++ HTMLForm/class.HTMLFormValidate.php
@@ -61,7 +61,7 @@ class HTMLFormValidate {
 			$d['error'] = __('Field "%1" must be set', $d['label'] ?: $field);
 		} elseif ($d['obligatory'] && !$value) {
 			$d['error'] = __('Field "%1" is obligatory', $d['label'] ?: $field);
-		} elseif (($type == 'email' || $field == 'email') && $value && !$this->validEmail($value)) {
+		} elseif (($type == 'email' || $field == 'email') && $value && !self::validEmail($value)) {
 			$d['error'] = __('Not a valid e-mail in field "%1"', $d['label'] ?: $field);
 		} elseif ($field == 'password' && strlen($value) < ifsetor($d['minlen'], 6)) {
 			$d['error'] = __('Password is too short. Min %s characters, please. It\'s for your own safety', ifsetor($d['minlen'], 6));
@@ -230,7 +230,7 @@ class HTMLFormValidate {
         $emailAddresses = preg_split('/\s*,\s*/', $value);
         foreach ($emailAddresses as &$emailAddress) {
             if ((class_exists('Swift_Validate') && !Swift_Validate::email($emailAddress)) ||
-                !self::validMail($value)) {
+                !self::validEmail($value)) {
                 $invalid[] = $emailAddress;
             }
         }
