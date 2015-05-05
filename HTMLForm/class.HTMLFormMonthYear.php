95780e7a1a2683a8b5a9532e4cfc60be064be8c6
Slawa@HP <Slawa@HP>
2015-05-05 22:02:40 +0200

    - FINISH filtering by work_title=* - ADD work_title parameters to Social page

--- HTMLForm/class.HTMLFormMonthYear.php
+++ HTMLForm/class.HTMLFormMonthYear.php
@@ -32,7 +32,7 @@ class HTMLFormMonthYear extends HTMLFormType {
 		$content[] = $this->showMonthOptions();
 		$content[] = '</select>';
 		$content[] = '</td><td style="padding-left: 1em;">';
-		$content[] = '<input name="'.$fieldStringY.'" value="'.$this->year.'" size="5" placeholder="год" class="form-control"/>';
+		$content[] = '<input type="number" name="'.$fieldStringY.'" value="'.$this->year.'" size="5" placeholder="год" class="form-control"/>';
 		$content[] = '</td></tr></table>';
 		return $content;
 	}
