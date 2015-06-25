f74c305e279e208dafb8dc525a32eac8ee7675e7
DEPIDSVY <depidsvy@nintendo.de>
2015-06-25 11:28:57 +0200

    - FIX NotifyVersion crashing - FIX DatabaseResultIterator not detecting the end of the results

--- ORM/class.OODBase.php
+++ ORM/class.OODBase.php
@@ -148,7 +148,7 @@ abstract class OODBase {
 			foreach ($this->idField as $field) {
 				$this->id[$field] = $this->data[$field];
 			}
-		} else if (ifsetor($this->data[$this->idField])) {
+		} else if (igorw\get_in($this->data, [$this->idField])) {   // not ifsetor
 			$this->id = $this->data[$this->idField];
 		} else {
 			debug(gettype($row), $this->idField, $this->data);
