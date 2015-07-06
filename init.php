9a42ede4bd554dcd220a9a925ee04004ac966df7
DEPIDSVY <depidsvy@nintendo.de>
2015-07-06 15:38:04 +0200

    - FIX login problems - FIX warnings

--- init.php
+++ init.php
@@ -320,4 +320,29 @@ if (!function_exists('nodebug')) {
 		return $tmp;
 	}
 
+	/**
+	 * http://php.net/manual/en/function.error-reporting.php#65884
+	 * @param $value
+	 * @return string
+	 */
+	function error2string($value) {
+		$level_names = array(
+			E_ERROR => 'E_ERROR', E_WARNING => 'E_WARNING',
+			E_PARSE => 'E_PARSE', E_NOTICE => 'E_NOTICE',
+			E_CORE_ERROR => 'E_CORE_ERROR', E_CORE_WARNING => 'E_CORE_WARNING',
+			E_COMPILE_ERROR => 'E_COMPILE_ERROR', E_COMPILE_WARNING => 'E_COMPILE_WARNING',
+			E_USER_ERROR => 'E_USER_ERROR', E_USER_WARNING => 'E_USER_WARNING',
+			E_USER_NOTICE => 'E_USER_NOTICE' );
+		if(defined('E_STRICT')) $level_names[E_STRICT]='E_STRICT';
+		$levels=array();
+		if(($value&E_ALL)==E_ALL)
+		{
+			$levels[]='E_ALL';
+			$value&=~E_ALL;
+		}
+		foreach($level_names as $level=>$name)
+			if(($value&$level)==$level) $levels[]=$name;
+		return implode(' | ',$levels);
+	}
+
 }
