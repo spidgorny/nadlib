b3abf27bef7cfea7874236850c69496cafdea60f
DEPIDSVY <depidsvy@nintendo.de>
2016-01-25 12:16:25 +0100

    - ADD ConstructORSTest - ADD Path::normalize()

--- HTTP/class.Path.php
+++ HTTP/class.Path.php
@@ -333,4 +333,40 @@ class Path {
 		return $new;
 	}
 
+	/**
+	 * http://php.net/manual/en/function.realpath.php#112367
+	 * @return string
+	 */
+	function normalize() {
+		$path = $this->__toString();
+		$parts = array();// Array to build a new path from the good parts
+		$path = str_replace('\\', '/', $path);// Replace backslashes with forwardslashes
+		$path = preg_replace('/\/+/', '/', $path);// Combine multiple slashes into a single slash
+		$segments = explode('/', $path);// Collect path segments
+		$test = '';// Initialize testing variable
+		foreach($segments as $segment)
+		{
+			if($segment != '.')
+			{
+				$test = array_pop($parts);
+				if(is_null($test))
+					$parts[] = $segment;
+				else if($segment == '..')
+				{
+					if($test == '..')
+						$parts[] = $test;
+
+					if($test == '..' || $test == '')
+						$parts[] = $segment;
+				}
+				else
+				{
+					$parts[] = $test;
+					$parts[] = $segment;
+				}
+			}
+		}
+		return implode('/', $parts);
+	}
+
 }
