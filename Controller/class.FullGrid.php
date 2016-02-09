2a47a12caec81082bf0d8f2f2e5d4462f958d7fc
DEPIDSVY <depidsvy@nintendo.de>
2016-02-09 15:24:38 +0100

    - ADD ViewMailLog having correct filter - FIX FullGrid showing better filters

--- Controller/class.FullGrid.php
+++ Controller/class.FullGrid.php
@@ -93,13 +93,31 @@ abstract class FullGrid extends Grid {
 	function getFilterWhere() {
 		$where = array();
 
+		$desc = $this->getFilterDesc();
+		//debug($desc);
 		foreach ($this->filter as $key => $val) {
 			if ($val) {
-				$where[$key] = $val;
+				$type = ifsetor($desc[$key]['type']);
+				list($field, $parameter) = $this->getFilterWherePair($key, $val, $type);
+				$where[$field] = $parameter;
 			}
 		}
 
 		return $where;
+	}
+
+	function getFilterWherePair($key, $val, $type) {
+		$where = [];
+		switch ($type) {
+			case 'like':
+				$like = new SQLLikeContains($val);
+				$where[$key] = $like;
+				break;
+			default:
+				$where[$key] = $val;
+				break;
+		}
+		return [$key, $val];
 	}
 
 	function sidebar() {
@@ -109,7 +127,8 @@ abstract class FullGrid extends Grid {
 	}
 
 	function getFilterForm(array $fields = NULL) {
-		$f = new HTMLFormTable($this->getFilterDesc($fields));
+		$desc = $this->getFilterDesc($fields);
+		$f = new HTMLFormTable($desc);
 		$f->setAllOptional();
 		$f->method('GET');
 		$f->defaultBR = true;
@@ -131,53 +150,17 @@ abstract class FullGrid extends Grid {
 	 */
 	function getFilterDesc(array $fields = NULL) {
 		$fields = ifsetor($fields, ifsetor($this->model->thes));
-		$fields = $fields ? $fields : $this->collection->thes;
+		$fields = $fields ?: $this->collection->thes;
 		$fields = is_array($fields) ? $fields : array();
 
 		//debug($this->filter);
 		$desc = array();
 		foreach ($fields as $key => $k) {
-			if (!is_array($k) ||
-				(is_array($k) && !ifsetor($k['noFilter']))
-			) {
-				$autoClass = ucfirst(str_replace('id_', '', $key)).'Collection';
-				if (class_exists($autoClass) &&
-					in_array('HTMLFormCollection', class_implements($autoClass))) {
-					$type = new $autoClass();
-					$options = NULL;
-				} elseif (!is_array($k) ||
-					(is_array($k) && ifsetor($k['tf']))
-				) {	// boolean
-					$type = 'select';
-					$stv = new slTableValue('', array());
-					$options = array(
-						't' => $stv->SLTABLE_IMG_CHECK,
-						'f' => $stv->SLTABLE_IMG_CROSS,
-					);
-					//debug($key, $this->filter[$key]);
-				} else {
-					$type = 'select';
-					if (!isset($k['options'])) {    // NOT ifsetor as we want to accept empty
-						$options = $this->getTableFieldOptions(ifsetor($k['dbField'], $key), false);
-						// convert to string for === operation
-						$options = ArrayPlus::create($options)->trim()->getData();
-					} else {
-						$options = $k['options'];
-					}
-					//debug($options);
-					// will only work for strings, ID to other table needs to avoid it
-					$options = array_combine_stringkey($options, $options);
-					//debug($options);
-				}
-				$desc[$key] = array(
-					'label' => is_array($k) ? $k['name'] : $k,
-					'type' => $type,
-					'options' => $options,
-					'null' => true,
-					'value' => ifsetor($this->filter[$key]),
-					'more' => 'class="input-medium"',
-					'===' => true,
-				);
+			if (!is_array($k)) {
+				$k = ['name' => $k];
+			}
+			if (!ifsetor($k['noFilter'])) {
+				$desc[$key] = $this->getFieldFilter($k, $key);
 			}
 		}
 		//debug($fields, $desc);
@@ -246,6 +229,56 @@ abstract class FullGrid extends Grid {
 		$this->collection->where = array_merge(
 			$this->collection->where,
 			$this->getFilterWhere()
+		);
+	}
+
+	/**
+	 * @param $k
+	 * @param $key
+	 * @return array
+	 */
+	public function getFieldFilter($k, $key) {
+		$autoClass = ucfirst(str_replace('id_', '', $key)) . 'Collection';
+		if (class_exists($autoClass) &&
+			in_array('HTMLFormCollection', class_implements($autoClass))
+		) {
+			$k['type'] = new $autoClass();
+			$options = NULL;
+		} elseif (ifsetor($k['tf'])) {    // boolean
+			$k['type'] = 'select';
+			$stv = new slTableValue('', array());
+			$options = array(
+				't' => $stv->SLTABLE_IMG_CHECK,
+				'f' => $stv->SLTABLE_IMG_CROSS,
+			);
+			//debug($key, $this->filter[$key]);
+		} elseif (ifsetor($k['type']) == 'select') {
+			if (!isset($k['options'])) {    // NOT ifsetor as we want to accept empty
+				$options = $this->getTableFieldOptions(ifsetor($k['dbField'], $key), false);
+				// convert to string for === operation
+				$options = ArrayPlus::create($options)->trim()->getData();
+			} else {
+				$options = $k['options'];
+			}
+			//debug($options);
+			// will only work for strings, ID to other table needs to avoid it
+			$options = array_combine_stringkey($options, $options);
+			//debug($options);
+		} elseif (ifsetor($k['type']) == 'like') {
+			// this is handled in getFilterWhere
+			$options = NULL;
+		} else {
+			$k['type'] = $k['type'] ?: 'input';
+			$options = NULL;
+		}
+		return array(
+			'label'   => $k['name'],
+			'type'    => $k['type'],
+			'options' => $options,
+			'null'    => true,
+			'value'   => ifsetor($this->filter[$key]),
+			'more'    => 'class="input-medium"',
+			'==='     => true,
 		);
 	}
 
