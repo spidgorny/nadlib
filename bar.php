392366a3a6eeaafcc90ae4e329ebf1fec25a4958
Slawa <Slawa@FWX1V32>
2016-04-03 02:17:47 +0300

    - FIX averaging of the ratings - ADD symetrical parameter to Bar image

--- bar.php
+++ bar.php
@@ -20,51 +20,91 @@
 
 require_once 'init.php';
 
-function drawRating($rating) {
-	$width = isset($_GET['width']) ? $_GET['width'] : 100;
-	$height = isset($_GET['height']) ? $_GET['height'] : 15;
-	$ratingbar = ($rating/100)*($width-5);
-	$barDX = 2;
-	$image = imagecreate($width,$height);
-	$color = isset($_GET['color']) ? $_GET['color'] : NULL;
-	$color = $color ? html2rgb($color) : array(0x43, 0xB6, 0xDF); #43B6DF
-	$fill = ImageColorAllocate($image, $color[0], $color[1], $color[2]);
-	//if ($rating > 49) { $fill = ImageColorAllocate($image,255,255,0); }
-	//if ($rating > 74) { $fill = ImageColorAllocate($image,255,128,0); }
-	//if ($rating > 89) { $fill = ImageColorAllocate($image,255,0,0); }
+class Bar {
 
-	$bg = isset($_GET['bg']) ? $_GET['bg'] : NULL;
-	$backColor = $bg ? html2rgb($bg) : array(0xFF, 0xFF, 0xFF);
-	$back = ImageColorAllocate($image, $backColor[0], $backColor[1], $backColor[2]);
-	$border = ImageColorAllocate($image,127,127,127);
-	ImageFilledRectangle($image,0,0,$width-1,$height-1,$back);
-	if (!ifsetor($_GET['!border'])) {
-		ImageRectangle($image,0,0,$width-1,$height-1,$border);
-	} else {
-		$ratingbar += 2;
-		$barDX = 0;
+	var $expires = 60*60*24*365;	// days
+
+	var $width;
+
+	var $height;
+
+	var $color;
+
+	var $backColor;
+
+	var $symmetric = false;
+
+	function __construct() {
+		$this->width = isset($_GET['width']) ? $_GET['width'] : 100;
+		$this->height = isset($_GET['height']) ? $_GET['height'] : 15;
+		$color = isset($_GET['color']) ? $_GET['color'] : NULL;
+		$this->color = $color ? $this->html2rgb($color) : array(0x43, 0xB6, 0xDF); #43B6DF
+		$bg = isset($_GET['bg']) ? $_GET['bg'] : NULL;
+		$this->backColor = $bg ? $this->html2rgb($bg) : array(0xFF, 0xFF, 0xFF);
+		$this->symmetric = ifsetor($_REQUEST['symmetric']);
 	}
-	ImageFilledRectangle($image, $barDX, $barDX, $barDX+$ratingbar, $height-$barDX-1, $fill);
-	imagePNG($image);
-	imagedestroy($image);
-}
 
-function html2rgb($color) {
-	if ($color[0] == '#')
-		$color = substr($color, 1);
+	function setHeaders() {
+		error_reporting(E_ALL);
+		//ini_set('display_errors', false);
+		header("Content-Type: image/png");
+		header("Pragma: public");
+		header("Cache-Control: maxage=".$this->expires);
+		header('Expires: ' . gmdate('D, d M Y H:i:s', time()+$this->expires) . ' GMT');
+	}
 
-	if (strlen($color) == 6)
-		list($r, $g, $b) = array($color[0].$color[1],
-			$color[2].$color[3],
-			$color[4].$color[5]);
-	elseif (strlen($color) == 3)
-		list($r, $g, $b) = array($color[0].$color[0], $color[1].$color[1], $color[2].$color[2]);
-	else
-		return false;
+	function drawRating($rating) {
+		$ratingbar = ($rating / 100) * ($this->width - 5);
+		$barDX = 2;
+		$image = imagecreate($this->width, $this->height);
 
-	$r = hexdec($r); $g = hexdec($g); $b = hexdec($b);
+		$backColor = $this->backColor;
+		$back = ImageColorAllocate($image, $backColor[0], $backColor[1], $backColor[2]);
+		$border = ImageColorAllocate($image, 127, 127, 127);
+		ImageFilledRectangle($image, 0, 0, $this->width - 1, $this->height - 1, $back);
 
-	return array($r, $g, $b);
+		if (!ifsetor($_GET['!border'])) {
+			ImageRectangle($image, 0, 0, $this->width - 1, $this->height - 1, $border);
+		} else {
+			$ratingbar += 2;
+			$barDX = 0;
+		}
+
+		$color = $this->color;
+		$fill = ImageColorAllocate($image, $color[0], $color[1], $color[2]);
+		//if ($rating > 49) { $fill = ImageColorAllocate($image,255,255,0); }
+		//if ($rating > 74) { $fill = ImageColorAllocate($image,255,128,0); }
+		//if ($rating > 89) { $fill = ImageColorAllocate($image,255,0,0); }
+		if ($this->symmetric) {
+			$middle = $this->width / 2;
+			ImageFilledRectangle($image, $middle + $barDX, $barDX, $middle + $barDX + $ratingbar/2, $this->height - $barDX - 1, $fill);
+		} else {
+			ImageFilledRectangle($image, $barDX, $barDX, $barDX + $ratingbar, $this->height - $barDX - 1, $fill);
+		}
+		imagePNG($image);
+		imagedestroy($image);
+	}
+
+	function html2rgb($color) {
+		if ($color[0] == '#')
+			$color = substr($color, 1);
+
+		if (strlen($color) == 6)
+			list($r, $g, $b) = array($color[0] . $color[1],
+					$color[2] . $color[3],
+					$color[4] . $color[5]);
+		elseif (strlen($color) == 3)
+			list($r, $g, $b) = array($color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2]);
+		else
+			return false;
+
+		$r = hexdec($r);
+		$g = hexdec($g);
+		$b = hexdec($b);
+
+		return array($r, $g, $b);
+	}
+
 }
 
 if (!function_exists('imagecreate')) {
@@ -73,12 +113,7 @@ if (!function_exists('imagecreate')) {
 	echo 'PHP: '.phpversion().'<br />';
 	echo 'GD not installed';
 } else {
-	error_reporting(E_ALL);
-	//ini_set('display_errors', false);
-	header("Content-Type: image/png");
-	$expires = 60*60*24*365;        // days
-	header("Pragma: public");
-	header("Cache-Control: maxage=".$expires);
-	header('Expires: ' . gmdate('D, d M Y H:i:s', time()+$expires) . ' GMT');
-	drawRating(min(100, intval($_GET['rating'])));
+	$bar = new Bar();
+	$bar->setHeaders();
+	$bar->drawRating(min(100, intval($_GET['rating'])));
 }
