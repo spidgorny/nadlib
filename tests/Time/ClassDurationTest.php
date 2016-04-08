9b8478d120d954d0f4d56361c6118643de7af2c4
debagsuy <debagsuy@8M4Z05J.corp.nintendo.eu>
2016-04-08 15:45:52 +0200

    unit test for class 'Duration'

--- tests/Time/ClassDurationTest.php
+++ tests/Time/ClassDurationTest.php
@@ -46,6 +46,7 @@ class ClassDurationTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('24h', $duration->short());
 	}
 
+	// check with slava
 	public function testFromHuman()
 	{
 		$result 	= Duration::fromHuman((60*30).'s');
@@ -56,6 +57,112 @@ class ClassDurationTest extends PHPUnit_Framework_TestCase
 
 		$result 	= Duration::fromHuman('1h 7m');
 		$this->assertEquals('01:07:00', $result->getTime());
+	}
+
+	public function testLessFalse()
+	{
+		$objTime = new Time();
+
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$objTime->setTime($date->getTimestamp());
+
+		$duration	= new Duration(time());
+		$this->assertFalse($duration->less($objTime));
+	}
+
+	public function testLessTrue()
+	{
+		$objTime = new Time();
+
+		$date = new DateTime('2022-01-01');
+		$date->setTime(10, 12, 15);
+
+		$objTime->setTime($date->getTimestamp());
+
+		$duration	= new Duration(time());
+		$this->assertTrue($duration->less($objTime));
+	}
+
+	public function testMoreTrue()
+	{
+		$objTime = new Time();
+
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$objTime->setTime($date->getTimestamp());
+
+		$duration	= new Duration(time());
+		$this->assertTrue($duration->more($objTime));
+	}
+
+	public function testMoreFalse()
+	{
+		$objTime = new Time();
+
+		$date = new DateTime('2022-01-01');
+		$date->setTime(10, 12, 15);
+
+		$objTime->setTime($date->getTimestamp());
+
+		$duration	= new Duration(time());
+		$this->assertFalse($duration->more($objTime));
+	}
+
+	public function testGetMinutes()
+	{
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$duration	= new Duration($date->getTimestamp());
+		$this->assertEquals('16305672.25', (string)$duration->getMinutes());
+	}
+
+	public function testGetHours()
+	{
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$duration	= new Duration($date->getTimestamp());
+		$this->assertEquals('271761.20416667', (string)$duration->getHours());
+	}
+
+	public function testGetDays()
+	{
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$duration	= new Duration($date->getTimestamp());
+		$this->assertEquals('11323.383506944', (string)$duration->getDays());
+	}
+
+	public function testGetRemHours()
+	{
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$duration	= new Duration($date->getTimestamp());
+		$this->assertEquals('271761', (string)$duration->getRemHours());
+	}
+
+	public function testGetRemMinutes()
+	{
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$duration	= new Duration($date->getTimestamp());
+		$this->assertEquals('12', (string)$duration->getRemMinutes());
+	}
+
+	public function testRemSeconds()
+	{
+		$date = new DateTime('2001-01-01');
+		$date->setTime(10, 12, 15);
+
+		$duration	= new Duration($date->getTimestamp());
+		$this->assertEquals('15', (string)$duration->getRemSeconds());
 	}
 
 }
