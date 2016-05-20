c194fce2041db2d78e27f5e0d7d5c753e017ba51
DEPIDSVY <depidsvy@nintendo.de>
2016-05-20 14:29:46 +0200

    - ADD Filter class dealing with Request/Preferences/Default - 0002625: People Mgmnt: Add option "Quick view" to Department drop down (it should be the one that displays the current initial view)

---
+++ Controller/Filter.php
@@ -0,0 +1,44 @@
+<?php
+
+class Filter extends ArrayObject {
+
+	protected $_preferences = [];
+
+	protected $_request = [];
+
+	protected $_default = [];
+
+	function __construct(array $input = array()) {
+		parent::__construct($input, ArrayObject::ARRAY_AS_PROPS);
+		$this->setRequest($input);
+	}
+
+	function setPreferences(array $_preferences) {
+		$this->_preferences = $_preferences;
+	}
+
+	function setRequest(array $_request) {
+		$this->_request = $_request;
+	}
+
+	function setDefault(array $_default) {
+		$this->_default = $_default;
+	}
+
+	function offsetGet($index) {
+		if (isset($this->_request[$index])) {
+			return $this->_request[$index];
+		} elseif (isset($this->_preferences[$index])) {
+			return $this->_preferences[$index];
+		} elseif (isset($this->_default[$index])) {
+			return $this->_default[$index];
+		}
+		return NULL;
+	}
+
+	function getArrayCopy() {
+		// first array has priority (only append new)
+		return $this->_request + $this->_preferences + $this->_default;
+	}
+
+}
