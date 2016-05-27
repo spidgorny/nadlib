c38ba916ea23c39feaf3e4a70a746d09aecf39ec
Slawa <Slawa@HP>
2016-05-28 01:07:25 +0200

    - ADD h5() to HTML

--- Controller/HTML.php
+++ Controller/HTML.php
@@ -65,6 +65,10 @@ class HTML {
 		return '<h4>'.$this->s($content).'</h4>';
 	}
 
+	function h5($content, array $more = array()) {
+		return '<h5 '.HTMLTag::renderAttr($more).'>'.$this->s($content).'</h5>';
+	}
+
 	function progress($percent) {
 		$percent = intval($percent);
 		return '<div class="progress">
