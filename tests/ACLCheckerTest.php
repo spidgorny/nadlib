c47423105a93cd0b42f75cdaabb9cda51521c754
DEPIDSVY <depidsvy@nintendo.de>
2016-06-02 13:40:32 +0200

    - FIX Cronjob access rights and handing of cronjob user enabled = 2

---
+++ tests/ACLCheckerTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DEPIDSVY
+ * Date: 25.02.2016
+ * Time: 19:07
+ */
+
+namespace tests;
+
+
+class ACLCheckerTest extends \PHPUnit_Framework_TestCase {
+
+	function test_compareACL() {
+		$controller = new \AppController();
+		$controller->user = NULL;
+		$ac = new \ACLChecker($controller);
+		$okNothing = $ac->compareACL(NULL);
+		$okNull = $ac->compareACL('null');
+		$okUser = $ac->compareACL('user');
+		debug(gettype2($controller->user), $okNothing, $okNull, $okUser);
+	}
+
+	function test_compareACLUser() {
+		$controller = new \AppController();
+		$controller->user = new \NOEUser();
+		$controller->user->id = 1;
+		$ac = new \ACLChecker($controller);
+		$okNothing = $ac->compareACL(NULL);
+		$okNull = $ac->compareACL('null');
+		$okUser = $ac->compareACL('user');
+		debug(gettype2($controller->user), $okNothing, $okNull, $okUser);
+	}
+
+}
