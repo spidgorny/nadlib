f5ba52a01e62b1b60cf5a3a5ac96c09bc2795be7
DEPIDSVY <depidsvy@nintendo.de>
2016-09-22 15:45:23 +0200

    - 0002944: Filtering for individuals to see a list of items which are currently assigned only to this person - ADD department overview of today's assignments

--- HTML/HTMLTag.php
+++ HTML/HTMLTag.php
@@ -75,6 +75,7 @@ class HTMLTag implements ArrayAccess {
 	function attr($name, $value = NULL) {
 		if ($value) {
 			$this->attr[$name] = $value;
+			return $this;
 		} else {
 			return ifsetor($this->attr[$name]);
 		}
@@ -82,6 +83,7 @@ class HTMLTag implements ArrayAccess {
 
 	function setAttr($name, $value) {
 		$this->attr[$name] = $value;
+		return $this;
 	}
 
 	function hasAttr($name) {
