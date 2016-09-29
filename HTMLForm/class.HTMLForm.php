bfb78dfae95b19d5cd26bee1358ba0dfb69652eb
DEPIDSVY <depidsvy@nintendo.de>
2016-09-29 14:11:49 +0200

    - ADD project selection to MoveProject

--- HTMLForm/class.HTMLForm.php
+++ HTMLForm/class.HTMLForm.php
@@ -207,13 +207,14 @@ class HTMLForm {
 	 * @see renderSelectionOptions
 	 */
 	function selection($name, array $aOptions, $default, $autoSubmit = FALSE, $more = '', $multiple = false, array $desc = array()) {
-		$this->stdout .= "<select ".$this->getName($name, $multiple ? '[]' : '');
+		$tag = $desc['tag'] ?: 'select';
+		$this->stdout .= "<$tag ".$this->getName($name, $multiple ? '[]' : '');
 		if ($autoSubmit) {
 			$this->stdout .= " onchange='this.form.submit()' ";
 		}
 		$this->stdout .= $more . ">\n";
 		$this->stdout .= $this->getSelectionOptions($aOptions, $default, $desc);
-		$this->stdout .= "</select>\n";
+		$this->stdout .= "</$tag>\n";
 	}
 
 	/**
