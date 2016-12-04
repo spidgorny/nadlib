9ecf67d43dce3448c4b74d4dd92dda5b3140a9c1
Slawa@HP <Slawa@HP>
2016-12-04 01:55:37 +0100

    - ADD FakeLogin cookie - ADD getUserDropDown() for the header

--- be/class/DB/AlterIndex.php
+++ be/class/DB/AlterIndex.php
@@ -59,7 +59,8 @@ class AlterIndex extends AppControllerBE {
 		foreach ($files as $file) {
 			/** @var $file SplFileInfo */
 			if ($file->getExtension() == 'json') {
-				$li[] = $this->a(new URL(get_class($this), array(
+				$li[] = $this->a(new URL(NULL, array(
+						'c' => get_class($this),
 						'file' => basename($file),
 					)), basename($file)) .
 					'<div style="float: right;">[' . date('Y-m-d H:i', $file->getCTime()) . ']</div>';
