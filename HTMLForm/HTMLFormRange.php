1ad631e6b9e2f02b06c924ebb9d57824f38f52e0
DEPIDSVY <depidsvy@nintendo.de>
2016-12-13 18:20:51 +0100

    - ADD HTMLFormRange for cronjob validity checking

---
+++ HTMLForm/HTMLFormRange.php
@@ -0,0 +1,48 @@
+<?php
+
+class HTMLFormRange extends HTMLFormType {
+
+	public $min = 0;
+
+	public $max = 1440;		// 24*60
+
+	public $value;
+
+	public $step = 1;
+
+	/**
+	 * @var AutoLoad
+	 */
+	var $al;
+
+	/**
+	 * @param string $field
+	 * @param integer $value
+	 */
+	function __construct($field, $value) {
+//		parent::__construct();
+		$this->field = $field;
+		$this->setValue($value);
+		$this->al = AutoLoad::getInstance();
+		$index = Index::getInstance();
+		$index->addJQuery();
+		$index->addJS($this->al->nadlibFromDocRoot.'HTMLForm/HTMLFormRange.js');
+	}
+
+	/**
+	 * @param int $value
+	 */
+	function setValue($value) {
+		$this->value = $value;
+	}
+
+	function render() {
+		$content = new View($this->al->nadlibRoot.'HTMLForm/HTMLFormRange.phtml', $this);
+		$fieldString = $this->form->getName($this->field, '', true);
+		$fieldString = str_replace('[', '\\[', $fieldString);
+		$fieldString = str_replace(']', '\\]', $fieldString);
+		$content->fieldEscaped = $fieldString;
+		return $content;
+	}
+
+}
