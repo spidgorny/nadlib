7b19e87608cc18664e8cfb3814f67942e7a91a61
DEPIDSVY <depidsvy@nintendo.de>
2017-03-15 14:29:58 +0100

    - FIX errors in free()

--- DB/Driver/dbLayerMS.php
+++ DB/Driver/dbLayerMS.php
@@ -271,8 +271,14 @@ AND name = '?')", array($table));
 	}
 
 	function free($res) {
+		@trigger_error('OK');
+		//error_clear_last();		// PHP 7.0
+
 		mssql_free_result($res);
-		if (error_get_last()) {
+
+		$error = error_get_last();
+		if ($error && $error['message'] != 'OK') {
+			debug(error_get_last());
 			debug_pre_print_backtrace();
 		}
 	}
