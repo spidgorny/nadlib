f91c1ac0aa961327b643937d5f1366e4c8675b7c
Slawa <Slawa@HP>
2017-03-17 04:31:41 +0100

    - FIX searching by city giving only limited amount of options

---
+++ Data/Proton.php
@@ -0,0 +1,35 @@
+<?php
+
+class Proton {
+
+	var $base = 'http://photon.komoot.de/api/?q=';
+
+	function get($q) {
+		$gz = new GuzzleHttp\Client();
+		$response = $gz->get($this->base . urlencode($q));
+		$json = $response->getBody()->getContents();
+		$features = json_decode($json);
+		return $features;
+	}
+
+	function getCities($q) {
+		$set = [];
+		$results = $this->get($q);
+		foreach ($results->features as $place) {
+			if (ifsetor($place->properties->name)) {
+				$set[] = $place->properties->name . ' (' . $place->properties->country . ')';
+			} elseif (ifsetor($place->properties->city)) {
+				$set[] = $place->properties->city . ' (' . $place->properties->country . ')';
+			} else {
+				debug($place->properties);
+			}
+		}
+		return $set;
+	}
+
+	public function getFirst($location) {
+		$results = $this->get($location);
+		return $results->features ? first($results->features) : NULL;
+	}
+
+}
