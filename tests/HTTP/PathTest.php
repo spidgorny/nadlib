f9f530415e4424b8f0acd265b070c5290435ff53
DEPIDSVY <depidsvy@nintendo.de>
2017-04-06 15:37:37 +0200

    - FIX AddRootDetector running from CLI - FIX Path->append()
    --HG--
    branch : recruitastic

--- tests/HTTP/PathTest.php
+++ tests/HTTP/PathTest.php
@@ -8,11 +8,34 @@
  */
 class PathTest extends PHPUnit_Framework_TestCase {
 
+	function test_Windows() {
+		$p = new Path(getcwd());
+		//debug($p->aPath, $p->sPath);
+		$this->assertGreaterThan(3, sizeof($p->aPath));
+		$this->assertEquals('C:', $p->aPath[0]);
+		$this->assertStringStartsWith('C:/', $p.'');
+	}
+
+	function test_cap_Windows() {
+		$source = cap(getcwd());
+		$p = new Path($source);
+//		debug($p->aPath, $p->sPath);
+		// all windows slash except last
+		$source = str_replace('\\', '/', getcwd()).'/';
+		$this->assertEquals($source, $p->implode());
+	}
+
 	function test_isAbsolute() {
 		$path = new Path('asd/qwe');
 		$this->assertFalse($path->isAbsolute());
 		$path = new Path('dev-jobz/Topic/hyperledger');
 		$this->assertFalse($path->isAbsolute());
+	}
+
+	function test_appRoot() {
+		$al = AutoLoad::getInstance();
+		$appRoot = $al->getAppRoot();
+		debug($appRoot.'');
 	}
 
 	function test_relativeFromAppRoot() {
@@ -21,11 +44,41 @@ class PathTest extends PHPUnit_Framework_TestCase {
 		$path = new Path($source);
 		$relative = $path->relativeFromAppRoot();
 		//debug($relative.'');
-		$this->assertTrue(in_array($relative.'', [
+		$this->assertContains($relative.'', [
 			'nadlib/'.$source,
 			'vendor/spidgorny/nadlib/'.$source,
 			'Users/DEPIDSVY/nadlib/'.$source,
-		]));
+			'tests/HTTP/'.$source,
+		]);
+	}
+
+	function test_append() {
+		$path = new Path('/var/www/htdocs/');
+		$path->append(new Path('ftp'));
+		$this->assertEquals('/var/www/htdocs/ftp', $path.'');
+	}
+
+	function test_append_capped() {
+		$path = new Path('/var/www/htdocs/');
+		$path->append(new Path('ftp/'));
+		$this->assertEquals('/var/www/htdocs/ftp/', $path.'');
+	}
+
+	function test_back_path() {
+		$path = new Path('../ftp');
+		$this->assertEquals(['..', 'ftp'], $path->aPath);
+	}
+
+	function test_append_back() {
+		$path = new Path('/var/www/htdocs/');
+		$path->append(new Path('../ftp'));
+		$this->assertEquals('/var/www/ftp', $path.'');
+	}
+
+	function test_append_back_twice() {
+		$path = new Path('/var/www/htdocs/');
+		$path->append(new Path('../../ftp'));
+		$this->assertEquals('/var/ftp', $path.'');
 	}
 
 }
