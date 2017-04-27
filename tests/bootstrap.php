0ceccc350d704c8aa1242a4065f9ab35a209ac58
DEPIDSVY <depidsvy@nintendo.de>
2017-04-27 18:37:55 +0200

    - ADD being able to select any person in a drop-down for HardwareTracking (old and new)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,10 @@
 
 require_once __DIR__.'/../bootstrap.php';
 require_once __DIR__.'/TestConfig.php';
-//class_alias(TestConfig::class, 'Config');
+class_alias(TestConfig::class, 'Config');
 Config::getInstance()->postInit();
+require_once __DIR__.'/AppController4Test.php';
+
+function __($a) { return $a; }
+
+echo basename(__DIR__).'/'.basename(__FILE__), ' done', BR;
