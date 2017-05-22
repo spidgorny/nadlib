1dd1e2b4e9932c603438651b6d0f460828ed2739
DEPIDSVY <depidsvy@nintendo.de>
2017-05-22 13:10:42 +0200

    - FIX wrong DB object used

--- Base/uTestBase.php
+++ Base/uTestBase.php
@@ -71,12 +71,13 @@ class uTestBase extends AppControllerBE
 		$dbt = debug_backtrace();
 		$i = 0;
 		$db = $dbt[$i];
-		while (in_array($db['function'], ['assert', 'assertNotEqual', 'assertTrue', 'assertGreaterThan'])) {
+		while (in_array($db['function'], ['assert', 'assertEqual', 'assertNotEqual', 'assertTrue', 'assertGreaterThan'])) {
 			$db = $dbt[++$i];
 		}
+		//Debug::peek($db);
 		//$row['file'] = basename($db1['file']);
 		$row['function'] = $db['function'];
-		$file = ifsetor($db['file']);
+		$file = ifsetor($db['file']) ? file($db['file']) : [];
 		$line = ifsetor($db['line']);
 		$row['line'] = ifsetor($file[$line]);
 
@@ -103,11 +104,11 @@ class uTestBase extends AppControllerBE
 
 		static $odd = 0;
 		if (Request::isCLI()) {
-			echo strip_tags(implode(TAB, $row)), BR;
+			return $row;
 		} else {
 			$content = '<tr class="' . ($odd++ % 2 ? 'odd' : '') . '"><td>' . implode('</td><td>', $row) . '</td></tr>';
+			return $content;
 		}
-		return $content;
 	}
 
 	function assert($bool)
@@ -135,22 +136,24 @@ class uTestBase extends AppControllerBE
 				$this->start = microtime(true);
 				ob_start();
 				$contentPlus = call_user_func([$this, $function]);
-				$this->echoBQ($contentPlus);
+				if ($contentPlus) {
+					echo '<< ', strip_tags(implode(TAB, $contentPlus)), BR;
+				}
 				$debug = ob_get_clean();
 				if ($debug) {
 					$this->echoBQ($debug);
 				}
-				echo number_format(microtime(true) - $this->start, 3, '.', ''), BR;
+				echo '<< ', number_format(microtime(true) - $this->start, 3, '.', ''), 'ms', BR;
 			}
 		}
 	}
 
 	function echoBQ($text)
 	{
-		$lines = trimExplode(PHP_EOL, $text);
+		$lines = explode(PHP_EOL, $text);
 		foreach ($lines as &$line) {
 			$line = str_repeat(' ', 8) . $line;
-			echo $line;
+			echo $line, BR;
 		}
 	}
 
