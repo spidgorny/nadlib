5c66e274235d5c045228574ac94d60d131a67ba0
Slawa <depidsvy@nintendo.de>
2017-07-27 16:06:22 +0200

    - ADD handling of $where in DBLayerJSONTable

--- DB/Driver/DBLayerJSONTable.php
+++ DB/Driver/DBLayerJSONTable.php
@@ -6,6 +6,8 @@ class DBLayerJSONTable extends DBLayerBase implements DBInterface {
 
 	var $data = [];
 
+	var $where = [];
+
 	function __construct($filename)
 	{
 		$this->filename = $filename;
@@ -23,14 +25,48 @@ class DBLayerJSONTable extends DBLayerBase implements DBInterface {
 			json_encode($this->data, JSON_PRETTY_PRINT));
 	}
 
+	public function __toString()
+	{
+		$query = 'SELECT * FROM '.basename($this->filename).' WHERE '.
+			json_encode($this->where);
+		//echo $query, BR;
+		return $query;
+	}
+
 	function fetchAll($res_or_query, $index_by_key = NULL)
 	{
-		return $this->data;
+		//return $this->data;
+		$data = [];
+		reset($this->data);
+		while (!is_null(key($this->data))) {
+			$newRow = $this->fetchAssoc($this->data);
+			//debug($newRow);
+			if (!empty($newRow)) {
+				$data[] = $newRow;
+			}
+			next($this->data);
+		}
+		return $data;
 	}
 
 	function fetchAssoc($res)
 	{
-		return current($this->data);
+		$row = null;
+		if ($this->where) {
+			while (!is_null(key($this->data))) {
+				$row = current($this->data);
+				if (self::matchWhere($row, $this->where)) {
+					return $row;
+				}
+				// only if false (next() is called in fetchAll())
+				next($this->data);
+			}
+			$row = null;	// last row was not returned
+		} else {
+			$row = current($this->data);
+		}
+//		debug($row, $this->where);
+		return $row;
 	}
 
 	function numRows($res = NULL)
@@ -55,12 +91,19 @@ class DBLayerJSONTable extends DBLayerBase implements DBInterface {
 	static function matchWhere(array $row, array $where)
 	{
 		foreach ($where as $key => $val) {
-			$matchOne = $row[$key] == $val;
+			$matchOne = (string)$row[$key] == (string)$val;
+			//debug($row[$key], $val, $matchOne);
 			if (!$matchOne) {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	function runSelectQuery($table, array $where = [], $order = '', $addSelect = null)
+	{
+		$this->where = $where;
+		return $this;
 	}
 
 }
