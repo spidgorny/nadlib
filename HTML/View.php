fe15b4d9e5a948091d37737e16b6a4a9c230ef11
Suyeb Bagdadi <suyeb.bagdadi@nintendo.de>
2018-04-17 18:05:47 +0200

    null check

--- HTML/View.php
+++ HTML/View.php
@@ -248,7 +248,10 @@ class View extends stdClass {
 
 	function data($key)
 	{
-		return $this->e(ifsetor($this->caller->data[$key]));
+		if ($this->caller != null) {
+			return $this->e($this->caller->get($key));
+		}
+		return null;
 	}
 
 	/**
@@ -260,7 +263,7 @@ class View extends stdClass {
 	function __toString()
 	{
 		if (DEVELOPMENT) {
-			debug($this->file, get_class($this->caller));
+			debug('Do not call View::__toString() as it will prevent you from obtaining a valid backtrace in case of an error.', $this->file, get_class($this->caller));
 			debug_pre_print_backtrace();
 		}
 //		return $this->render().'';
