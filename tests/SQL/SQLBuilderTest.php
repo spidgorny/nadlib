16c610f3d43fc6c16df7903bb89fdfc43939f9c2
Slawa <depidsvy@nintendo.de>
2018-06-21 17:41:42 +0200

    - fix 100% nadlib tests

--- tests/SQL/SQLBuilderTest.php
+++ tests/SQL/SQLBuilderTest.php
@@ -1,14 +1,20 @@
 <?php
 
-class SQLBuilderTest extends PHPUnit_Framework_TestCase {
+class SQLBuilderTest extends PHPUnit_Framework_TestCase
+{
 
-	var $db;
+	/**
+	 * @var DBInterface
+	 */
+	protected $db;
 
-	function setUp() {
+	public function setUp()
+	{
 		$this->db = Config::getInstance()->getDB();
 	}
 
-	function test_getSelectQuery() {
+	public function test_getSelectQuery()
+	{
 		$qb = new SQLBuilder($this->db);
 		$query = $qb->getSelectQueryString('table', [
 			'a' => 'b',
@@ -16,22 +22,22 @@ class SQLBuilderTest extends PHPUnit_Framework_TestCase {
 		$must = "SELECT \"table\".*
 FROM \"table\"
 WHERE
-a = 'b'
+\"a\" = 'b'
 ORDER BY c";
 		$must = str_replace("\r\n", "\n", $must);
 		debug($must, $query);
 		$this->assertEquals($must, $query);
 	}
 
-	function test_getSelectQueryP() {
-		$qb = new SQLBuilder($this->db);
-		$query = $qb->getSelectQueryP('table', [
+	public function test_getSelectQueryP()
+	{
+		$query = SQLSelectQuery::getSelectQueryP($this->db, 'table', [
 			'a' => new SQLLikeContains('b'),
 		], 'ORDER BY c');
 		$must = "SELECT \"table\".*
 FROM \"table\"
 WHERE
-a ILIKE '%' || $1 || '%'
+\"a\" ILIKE '%' || $1 || '%'
 ORDER BY c";
 		$must = $this->implodeSQL($must);
 		$sQuery = $query->getQuery();
@@ -40,7 +46,8 @@ ORDER BY c";
 		$this->assertEquals($must, $sQuery);
 	}
 
-	function implodeSQL($sql) {
+	public function implodeSQL($sql)
+	{
 		$sql = strtr($sql, [
 			" " => '',
 			"\t" => '',
