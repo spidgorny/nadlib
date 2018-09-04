a86c34cafe89587d3563265ac693eee8f0d171a7
Slawa <depidsvy@nintendo.de>
2018-09-04 13:53:00 +0200

    - fix NotifyHWConfirmation crashing in CLI - add DumpRequest exporting to CSV directly - add analysis of requests
    --HG--
    branch : ors-master

--- HTTP/CLIResolver.php
+++ HTTP/CLIResolver.php
@@ -1,12 +1,16 @@
 <?php
 
-class CLIResolver implements ResolverInterface {
+class CLIResolver implements ResolverInterface
+{
 
-	function getController() {
+	function getController()
+	{
 		$request = Request::getInstance();
 		$argv = array_filter($_SERVER['argv'], function ($el) {
-			return !str_startsWith($el, '--');	// remove params
+			return !str_startsWith($el, '--');    // remove params
 		});
+		unset($argv[0]);	// path to the index.php file
+//		pre_print_r($_SERVER['argv'], $argv);
 		$controller = sizeof($argv) ? first($argv) : null;
 		if (DEVELOPMENT && $controller) {
 //			echo 'ArgV: ', implode(' ', $_SERVER['argv']);
