d0686716c538949039ed7e1fb565186e05b78dce
Slawa Pidgorny <spidgorny@gmail.com>
2018-11-26 11:39:52 +0100

    - add test for render() functions everywhere - remove global isAdmin() function - extract People class (probably not used) - extract Users

--- Base/ConfigBase.php
+++ Base/ConfigBase.php
@@ -223,14 +223,23 @@ class ConfigBase implements ConfigInterface
 		}
 	}
 
+	/**
+	 * @return LoginUser|User|UserModelInterface
+	 * @throws LoginException
+	 */
 	function getUser()
 	{
-		return NULL;
+		if (is_object($this->user)) {
+			return $this->user;
+		} else {
+			throw new LoginException(__METHOD__);
+		}
 	}
 
 	/**
 	 * Convenience function example how to use Login
 	 * @return LoginUser|User
+	 * @throws DatabaseException
 	 */
 	function _getLoginUser()
 	{
