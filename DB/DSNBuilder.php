9e43b99081d36131ce02edb51b96a8d7486ddc17
Slawa <depidsvy@nintendo.de>
2019-03-11 13:36:48 +0100

    - merge and remove redundant classes from nin_pbl - add TestConfig for testing

--- DB/DSNBuilder.php
+++ DB/DSNBuilder.php
@@ -1,26 +1,25 @@
 <?php
 
-class DSNBuilder {
+class DSNBuilder
+{
 
-	public static function make($scheme, $host, $user, $pass, $db, $port) {
+	public static function make($scheme, $host, $user, $pass, $db, $port)
+	{
 		$classMap = [
-//			'mysql' => DSNBuilderMySQL::class,
-			'mysql' => 'DSNBuilderMySQL',
-//			'pgsql' => DSNBuilderPostgreSQL::class,
-			'pgsql' => 'DSNBuilderPostgreSQL',
-//			'sqlite' => DSNBuilderSQLite::class,
-			'sqlite' => 'DSNBuilderSQLite',
-//			'mssql' => DSNBuilderMSSQL::class,
-			'mssql' => 'DSNBuilderMSSQL',
+			'mysql' => DSNBuilderMySQL::class,
+			'pgsql' => DSNBuilderPostgreSQL::class,
+			'sqlite' => DSNBuilderSQLite::class,
+			'mssql' => DSNBuilderMSSQL::class,
 		];
 		$dsnClass = $classMap[$scheme];
 		$builder = new $dsnClass($host, $user, $pass, $db, $port);
 		return $builder;
 	}
 
-	function getDSN(array $params) {
+	function getDSN(array $params)
+	{
 		$url = http_build_query($params, NULL, ';', PHP_QUERY_RFC3986);
-		$url = str_replace('%20', ' ', $url);	// back convert
+		$url = str_replace('%20', ' ', $url);    // back convert
 		$url = urldecode($url);
 		return $url;
 	}
