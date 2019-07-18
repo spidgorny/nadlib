9ae0d5d3796114a1ff248738de5f478c1942adbd
depidsvy <depidsvy@Locals-MacBook-Pro.local>
2019-07-18 16:46:08 +0200

    - refactor to allow public controllers for API

--- Controller/JsonController.php
+++ Controller/JsonController.php
@@ -11,34 +11,40 @@ trait JsonController
 
     public function __invoke()
     {
-//        debug($_SERVER);
-        $requestURI = ifsetor($_SERVER['REQUEST_URI']);
-        $url = new \spidgorny\nadlib\HTTP\URL($requestURI);
-        $levels = $url->getPath()->getLevels();
-
-        // next after /API/
-        //llog(get_class($this));
-        $last = null;
-        $arguments = [];
-        foreach ($levels as $i => $el) {
-            if ($el == get_class($this)) {
-                $last = ifsetor($levels[$i+1]);
-                $arguments = array_slice($levels, $i+2);    // rest are args
-                break;
-            }
-        }
-        if (!$last) {
-            $last = last($levels);
-        }
-        $request = trim($last, '/\\ ');
+		list($request, $arguments) = $this->getActionAndArguments();
         return call_user_func_array([$this, $request], $arguments);
     }
 
-    public function error(Exception $e)
+    public function getActionAndArguments()
+	{
+		//        debug($_SERVER);
+		$requestURI = ifsetor($_SERVER['REQUEST_URI']);
+		$url = new \spidgorny\nadlib\HTTP\URL($requestURI);
+		$levels = $url->getPath()->getLevels();
+
+		// next after /API/
+		//llog(get_class($this));
+		$last = null;
+		$arguments = [];
+		foreach ($levels as $i => $el) {
+			if ($el == get_class($this)) {
+				$last = ifsetor($levels[$i+1]);
+				$arguments = array_slice($levels, $i+2);    // rest are args
+				break;
+			}
+		}
+		if (!$last) {
+			$last = last($levels);
+		}
+		$request = trim($last, '/\\ ');
+		return [$request, $arguments];
+	}
+
+    public function error(Exception $e, $httpCode = 500)
     {
         $message = '[' . get_class($e) . ']' . PHP_EOL . $e->getMessage() . PHP_EOL . $e->getFile() . '#' . $e->getLine();
         llog($message);
-        http_response_code(500);
+        http_response_code($httpCode);
         header('Content-Type: application/json');
         return json_encode([
             'status' => 'error',
