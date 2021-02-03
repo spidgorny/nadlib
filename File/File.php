01cf8818bae81c4de350d77179776dc98b4288ef
Slawa <depidsvy@nintendo.de>
2021-02-03 13:45:54 +0100

    - switch scandir from flysystem to scandir()

--- File/File.php
+++ File/File.php
@@ -5,17 +5,41 @@ class File
 
 	protected $dir;
 
+	public bool $isDir;
+
 	protected $name;
 
-	public $spl;
+	public SplFileInfo $spl;
 
+	/**
+	 * @var \League\Flysystem\Filesystem
+	 */
 	public $fly;
 
+	/**
+	 * @var array[
+	 * 'type'=>'file',
+	 * 'path'=>'asd.ext',
+	 * 'timestamp'=>1234567890
+	 * 'size'=>1234,
+	 * 'dirname'=>string,
+	 * 'basename'=>string,
+	 * 'extension'=>'ext',
+	 * 'filename'=>'asd',
+	 * ]
+	 */
 	public $meta;
 
 	public static function fromLocal($file)
 	{
-		return new static($file);
+		if (!file_exists($file) && !is_dir($file)) {
+			throw new Exception('File ' . $file . ' does not exists');
+		}
+		$file = new static($file);
+		if (is_dir($file)) {
+			$file->isDir = true;
+		}
+		return $file;
 	}
 
 	public static function fromSpl(SplFileInfo $info)
@@ -35,7 +59,7 @@ class File
 
 	public function __construct($path)
 	{
-		$this->dir = dirname($path);
+		$this->dir = dirname($path) === '.' ? '' : dirname($path);
 		$this->name = basename($path);
 	}
 
@@ -61,7 +85,10 @@ class File
 
 	public function getPathname()
 	{
-		return $this->dir.'/'.$this->name;
+		if ($this->dir) {
+			return $this->dir . '/' . $this->name;
+		}
+		return $this->name;
 	}
 
 	public function md5()
@@ -114,6 +141,11 @@ class File
 	public function getMTime()
 	{
 		return filemtime($this->getPathname());
+	}
+
+	public function getType()
+	{
+		return $this->isDir ? 'dir' : 'file';
 	}
 
 }
