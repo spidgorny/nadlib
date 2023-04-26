d3a67c0ae814726bdedde33b6e5ea5d868169ebc
Slawa <depidsvy@nintendo.de>
2023-04-26 15:32:13 +0200

    PHP-708 ORS-TEST: Dashboard looks a bit odd with the messages

--- static.php
+++ static.php
@@ -1,7 +1,13 @@
-<?php
+<?php /** @noinspection ALL */
 
 // this file is used to help phpstan understand some undefined constants
 
-define('BR', "<br />\n");
-define('TAB', "\t");
-define('DEVELOPMENT', true);
+if (!defined('BR')) {
+	define('BR', "<br />\n");
+}
+if (!defined('TAB')) {
+	define('TAB', "\t");
+}
+if (!defined('DEVELOPMENT')) {
+	define('DEVELOPMENT', true);
+}
