12474354cb220ecbd73b235449358b8719aaf6c5
Slawa <depidsvy@nintendo.de>
2023-05-10 10:25:44 +0200

    - fix RSS for RIS

---
+++ tests/Controller/SelfServiceControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controller;
+
+use AppBundle\Controller\SelfServiceController;
+use PHPUnit\Framework\TestCase;
+use TestConfig;
+
+class SelfServiceControllerTest extends TestCase
+{
+
+	public function setUp()
+	{
+		chdir(dirname(__FILE__).'/../../../../../');
+	}
+
+	public function testGetRssScripts()
+	{
+		$tc = TestConfig::getInstance();
+		$ssc = new SelfServiceController();
+		$tag = $ssc->getRssScripts();
+		self::assertStringStartsWith('<script src', $tag);
+	}
+
+	public function testGetRssStyles()
+	{
+		$tc = TestConfig::getInstance();
+		$ssc = new SelfServiceController();
+		$tag = $ssc->getRssStyles();
+		self::assertStringStartsWith('<link rel', $tag);
+	}
+}
