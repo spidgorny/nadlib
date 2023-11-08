176f2a7b15698da867fbcce11226b1c4eac56c96
Slawa <depidsvy@nintendo.de>
2023-11-08 19:15:29 +0100

    - fix cache by using json

--- Cache/MemCacheRedis.php
+++ Cache/MemCacheRedis.php
@@ -12,12 +12,12 @@ class MemCacheRedis implements MemcacheInterface
 
 	public function get($key)
 	{
-		return $this->predis->get($key);
+		return json_decode($this->predis->get($key), false, 512 /*JSON_THROW_ON_ERROR*/);
 	}
 
 	public function set($key, $val)
 	{
-		return $this->predis->set($key, $val);
+		return $this->predis->set(json_encode($key), $val);
 	}
 
 	public function isValid($key = null, $expire = 0)
