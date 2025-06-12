9cc8420e20808cdd8b28109b489f2e7c335ff645
Slawa Pidgorny <depidsvy@nintendo.de>
2025-06-12 22:04:10 +0200

    fix: phpstan 2

--- User/Department.php
+++ User/Department.php
@@ -1,17 +1,8 @@
 <?php
 
-use App\Security\Legacy\RisAccessRights;
-
 class Department extends OODBase
 {
 
 	public $table = 'departments';
 
-
-	// @todo: move to RIS
-	public function getRights()
-	{
-//		return new RisAccessRights(null, $this->db);
-	}
-	
 }
