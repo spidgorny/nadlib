f9b4978d7d7dfc14111e4d00d895994bbe6a2e55
Slawa Pidgorny <depidsvy@nintendo.de>
2025-06-12 22:06:57 +0200

    fix: phpstan 2

--- User/Department.php
+++ User/Department.php
@@ -5,4 +5,17 @@ class Department extends OODBase
 
 	public $table = 'departments';
 
+	public function getRights()
+	{
+		return new AccessRights(null, $this->db);
+	}
+
+	/**
+	 * @return Person[]
+	 */
+	public function getPeople()
+	{
+		return [];
+	}
+
 }
