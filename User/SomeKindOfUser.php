4cd5e17e9aaeb5cfffe15d0049dade177dc8243a
Slawa Pidgorny <depidsvy@nintendo.de>
2025-06-18 12:57:54 +0200

    fix: phpstan

---
+++ User/SomeKindOfUser.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * A user not backed by a database.
+ * It may come from HTTP login/password or be anonymous user from CLI.
+ * Should not have any persistence methods. Only getters.
+ */
+interface SomeKindOfUser
+{
+
+	public function isAuth();
+
+	public function isAdmin();
+
+	public function getLogin();
+
+	/**
+	 * @param string $acl
+	 * @return bool
+	 */
+	public function can($acl);
+
+	public function getID();
+
+	public function getData(): array;
+
+	public function getGravatarURL($size = 32);
+
+	public function getName(): string;
+
+}
